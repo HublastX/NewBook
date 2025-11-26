13174f94d555b79232a74479e57de90b90b425cc
gabrieldevreis <gabrielreisdev@gmail.com>
2025-11-25 22:12:39 -0300

    feat(routes): adiciona rota para o layout com componente principal

--- frontend/newbook/src/app/app-routing.module.ts
+++ frontend/newbook/src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { LayoutComponent } from './features/layout/layout.component';
+import { MainComponent } from './features/layout/main/main.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'layout', component: LayoutComponent },
+
+  { path: 'home', 
+    component: LayoutComponent,
+    children:[
+      {
+        path: 'initial',
+        component:MainComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
