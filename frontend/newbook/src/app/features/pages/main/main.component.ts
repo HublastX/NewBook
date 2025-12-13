84669d52f95afc1fa7763fee9783cedd6353cacc
gabrieldevreis <gabrielreisdev@gmail.com>
2025-12-12 21:29:39 -0300

    feat(main): atualiza layout e lógica do componente principal

--- frontend/newbook/src/app/features/pages/main/main.component.ts
+++ frontend/newbook/src/app/features/pages/main/main.component.ts
@@ -4,18 +4,36 @@ import { CardBook } from '../../../interface/card-book.interface';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
 export class MainComponent {
-  
- bookExample: CardBook = {
-  title: 'O Hobbit',
-  author: 'J.R.R. Tolkien',
-  category: 'Fantasia',
-  synopsis: 'Bilbo embarca em uma aventura inesperada...',
-  likes: 1200,
-  stars: 5,
-  status: 'Disponível'
-};
-
+  bookExample: CardBook[] = [
+    {
+      title: 'O Hobbit',
+      author: 'J.R.R. Tolkien',
+      category: 'Fantasia',
+      synopsis: 'Bilbo embarca em uma aventura inesperada...',
+      likes: 1200,
+      stars: 5,
+      status: 'Disponível',
+    },
+    {
+      title: '1984',
+      author: 'George Orwell',
+      category: 'Distopia',
+      synopsis: 'Um clássico sobre vigilância e controle social...',
+      likes: 900,
+      stars: 4,
+      status: 'Raro',
+    },
+    {
+      title: 'Dom Quixote',
+      author: 'Miguel de Cervantes',
+      category: 'Romance',
+      synopsis: 'As aventuras do cavaleiro da triste figura...',
+      likes: 700,
+      stars: 5,
+      status: 'Disponível',
+    },
+  ];
 }
