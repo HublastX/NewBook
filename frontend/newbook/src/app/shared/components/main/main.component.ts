c6166f958c255dacf0852c39fe75cc89e84b1498
gabrieldevreis <gabrielreisdev@gmail.com>
2025-12-16 10:23:58 -0300

    feat: adicionar nova estrutura de pages e shared

---
+++ frontend/newbook/src/app/shared/components/main/main.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CardBook } from '../../../shared/interface/card-book.interface';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
+})
+export class MainComponent {
+  bookExample: CardBook[] = [
+    {
+      title: 'O Hobbit',
+      author: 'J.R.R. Tolkien',
+      category: 'Fantasia',
+      synopsis: 'Bilbo embarca em uma aventura inesperada...',
+      likes: 1200,
+      stars: 5,
+      status: 'Disponível',
+    },
+    {
+      title: '1984',
+      author: 'George Orwell',
+      category: 'Distopia',
+      synopsis: 'Um clássico sobre vigilância e controle social...',
+      likes: 900,
+      stars: 4,
+      status: 'Raro',
+    },
+    {
+      title: 'Dom Quixote',
+      author: 'Miguel de Cervantes',
+      category: 'Romance',
+      synopsis: 'As aventuras do cavaleiro da triste figura...',
+      likes: 700,
+      stars: 5,
+      status: 'Disponível',
+    },
+  ];
+}
