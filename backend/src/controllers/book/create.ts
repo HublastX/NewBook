b26cfdb0b8dd036c6bf0ed6aff4899d0d783dd7e
Rafaelgbi <rafa-ibg@outlook.com>
2026-02-01 20:44:56 -0300

    feat(controller_book):  creating new rules in the create book

--- backend/src/controllers/book/create.ts
+++ backend/src/controllers/book/create.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from "express";
+import { ZodError } from "zod"
+import { createBookSchema } from "../../schema/book/book.schema";
+import { bookService } from "../../services/book/book.service";
 
 export async function createBook(req:Request, res: Response){
 
     try{
+        const data = createBookSchema.parse(req.body);
+        const book = await bookService.create(data);
 
+        return res.status(201).json(book);
     }catch(error:any){
+        if (error instanceof ZodError){
+            return res.status(400).json({
+                error: "Dados invalidos",
+                details: error.flatten().fieldErrors,
+            });
+        }
 
+        console.error(error);
+
+        return res.status(500).json({
+            error: "Erro interno do servidor",
+        });
     };
 
 };
