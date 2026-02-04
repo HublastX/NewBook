aafc5e7a85838b61b1773687ddef56aa2f55e631
Rafaelgbi <rafa-ibg@outlook.com>
2026-02-03 21:36:49 -0300

    feat(book_schema): update book validation rules

--- backend/src/schema/book/book.schema.ts
+++ backend/src/schema/book/book.schema.ts
@@ -1,10 +1,10 @@
 import {z} from 'zod';
 
 export const createBookSchema = z.object({
-    title: z.string().min(1,'Título é obrigatório'),
-    author: z.string().min(1, 'Autor é obrigatório'),
-    category: z.string().min(1, 'Categoria é obrigatória'),
-    synopsis: z.string().min(10, "Sinopse deve ter pelo menos 10 caracteres"),
+    title: z.string('titulo é obrigatório').min(3,'Título deve ter no mínimo 3 caracteres'),
+    author: z.string('Autor é obrigatório').min(3, 'Autor deve ter no mínimo 3 caracteres'),
+    category: z.string('Categoria é obrigatória').min(3, 'Categoria deve ter no mínimo 3 caracteres'),
+    synopsis: z.string('Sinopse é obrigatória').min(10, "Sinopse deve ter pelo menos 10 caracteres"),
 });
 
 export type createBookDTO = z.infer<typeof createBookSchema>;
